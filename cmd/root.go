940f36891a9af0348b3b499b6021fc6d35e31ef6
Spencer Brown <spencer@privategrity.com>
2018-05-08 09:24:26 -0700

    Initial commit

---
+++ cmd/root.go
@@ -0,0 +1,81 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2018 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
+
+// Package cmd initializes the CLI and config parsers as well as the logger.
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	jww "github.com/spf13/jwalterweatherman"
+	"github.com/spf13/viper"
+	"os"
+)
+
+var verbose bool
+
+// Execute adds all child commands to the root command and sets flags
+// appropriately.  This is called by main.main(). It only needs to
+// happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		jww.ERROR.Println(err)
+		os.Exit(1)
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "gateway",
+	Short: "Run a gateway between cMix clients and servers",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Block forever as a keepalive
+		quit := make(chan bool)
+		<-quit
+	},
+}
+
+// init is the initialization function for Cobra which defines commands
+// and flags.
+func init() {
+	// NOTE: The point of init() is to be declarative.
+	// There is one init in each sub command. Do not put variable declarations
+	// here, and ensure all the Flags are of the *P variety, unless there's a
+	// very good reason not to have them as local params to sub command."
+	cobra.OnInitialize(initConfig, initLog)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false,
+		"Verbose mode for debugging")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {}
+
+// initLog initializes logging thresholds and the log path.
+func initLog() {
+	// If verbose flag set then log more info for debugging
+	if verbose || viper.GetBool("verbose") {
+		jww.SetLogThreshold(jww.LevelDebug)
+		jww.SetStdoutThreshold(jww.LevelDebug)
+	} else {
+		jww.SetLogThreshold(jww.LevelInfo)
+		jww.SetStdoutThreshold(jww.LevelInfo)
+	}
+	if viper.Get("logPath") != nil {
+		// Create log file, overwrites if existing
+		logPath := viper.GetString("logPath")
+		logFile, err := os.Create(logPath)
+		if err != nil {
+			jww.WARN.Println("Invalid or missing log path, default path used.")
+		} else {
+			jww.SetLogOutput(logFile)
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2018 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
+
+package main
+
+import "gitlab.com/privategrity/gateway/cmd"
+
+// main needs no introduction.
+func main() {
+	cmd.Execute()
+}
