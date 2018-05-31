3d9494915dd9cc2804101724ab821a0441329f1b
Richard T. Carback III <rick.carback@gmail.com>
2018-05-31 16:49:43 +0000

    Update comments for the storage interface

--- storage/interface.go
+++ storage/interface.go
@@ -12,11 +12,19 @@ import (
 
 // Interface for interacting with the MessageBuffer (in prep for a db impl)
 type MessageBuffer interface {
+	// StartMessageCleanup is a thread that auto-removes old messages
 	StartMessageCleanup(msgTimeout int)
+	// GetMessage returns a given message for a specific user
 	GetMessage(userId uint64, msgId string) (*pb.CmixMessage, bool)
-	GetMessageIDs(userId uint64) ([]string, bool)
+	// GetMessageIDs returns the mesage IDs received after the given messageID, or
+	// all message IDs if that ID cannot be found or is empty
+	GetMessageIDs(userId uint64, messageID string) ([]string, bool)
+	// DeleteMessage deletes a specific message
 	DeleteMessage(userId uint64, msgId string)
+	// AddMessage adds a message to the buffer for a specific user
 	AddMessage(userId uint64, msgId string, msg *pb.CmixMessage)
+	// AddOutGoingMessage adds a message to send to the cMix node
 	AddOutgoingMessage(msg *pb.CmixMessage)
+	// PopOutgoingBatch sends a batch of messages to the cMix node
 	PopOutgoingBatch(batchSize uint64) []*pb.CmixMessage
 }
