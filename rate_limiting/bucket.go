42cb6795615eaf8b1b787850caf6ada59b1643c6
Jono <jono@privategrity.com>
2019-02-19 11:12:33 -0800

    Fix the Add() function to correctly calculate remaining tokens.

--- rate_limiting/bucket.go
+++ rate_limiting/bucket.go
@@ -6,36 +6,53 @@ import (
 )
 
 type Bucket struct {
-	capacity   uint      // maximum number of tokens the bucket can hold
-	remaining  uint      // current number of tokens in the bucket
+	capacity   int64     // maximum number of tokens the bucket can hold
+	remaining  int64     // current number of tokens in the bucket
+	rate       int64     // rate that the bucket leaks at
 	lastUpdate time.Time // last time the bucket was updated
 	mux        sync.Mutex
 }
 
 // Generates a new empty bucket.
-func Create(capacity uint) Bucket {
-	return Bucket{capacity: capacity, remaining: capacity, lastUpdate: time.Now()}
+func Create(capacity int64, rate int64) Bucket {
+	return Bucket{
+		capacity:   capacity,
+		remaining:  capacity,
+		rate:       rate,
+		lastUpdate: time.Now(),
+	}
 }
 
 // Returns the capacity of the bucket.
-func (b *Bucket) Capacity() uint {
+func (b *Bucket) Capacity() int64 {
 	return b.capacity
 }
 
 // Returns the remaining space in the bucket.
-func (b *Bucket) Remaining() uint {
+func (b *Bucket) Remaining() int64 {
 	return b.remaining
 }
 
-// Add token to the bucket. Returns true if token was added and false if there
-// was insufficient capacity to do so.
+// Add token to the bucket and updates the remaining number of tokens. Returns
+// true if token was added; false if there was insufficient capacity to do so.
 func (b *Bucket) Add(token uint) bool {
 	b.mux.Lock()
 
-	if b.remaining == 0 {
+	// Update the amount remaining by calculating the number of leaks
+	b.remaining -= time.Now().Sub(b.lastUpdate).Nanoseconds() * b.rate
+
+	// Ensure remaining is no less than zero
+	if b.remaining < 0 {
+		b.remaining = 0
+	}
+
+	b.lastUpdate = time.Now()
+
+	if b.remaining >= b.capacity {
+		b.mux.Unlock()
 		return false
 	} else {
-		b.remaining--
+		b.mux.Unlock()
 		return true
 	}
 }
