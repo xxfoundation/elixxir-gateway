1d65aa0d71e0af6798b569c94017b0fb982fd92a
Jono <jono@privategrity.com>
2019-02-19 15:21:18 -0800

    Add tests for Create(), Capacity(), Remaining(), and Add()

--- rate_limiting/bucket.go
+++ rate_limiting/bucket.go
@@ -6,15 +6,15 @@ import (
 )
 
 type Bucket struct {
-	capacity   int64     // maximum number of tokens the bucket can hold
-	remaining  int64     // current number of tokens in the bucket
-	rate       int64     // rate that the bucket leaks at
+	capacity   uint      // maximum number of tokens the bucket can hold
+	remaining  uint      // current number of tokens in the bucket
+	rate       float64   // rate that the bucket leaks at [leaks/nanosecond]
 	lastUpdate time.Time // last time the bucket was updated
 	mux        sync.Mutex
 }
 
 // Generates a new empty bucket.
-func Create(capacity int64, rate int64) Bucket {
+func Create(capacity uint, rate float64) Bucket {
 	return Bucket{
 		capacity:   capacity,
 		remaining:  capacity,
@@ -24,12 +24,12 @@ func Create(capacity int64, rate int64) Bucket {
 }
 
 // Returns the capacity of the bucket.
-func (b *Bucket) Capacity() int64 {
+func (b *Bucket) Capacity() uint {
 	return b.capacity
 }
 
 // Returns the remaining space in the bucket.
-func (b *Bucket) Remaining() int64 {
+func (b *Bucket) Remaining() uint {
 	return b.remaining
 }
 
@@ -38,8 +38,14 @@ func (b *Bucket) Remaining() int64 {
 func (b *Bucket) Add(token uint) bool {
 	b.mux.Lock()
 
-	// Update the amount remaining by calculating the number of leaks
-	b.remaining -= time.Now().Sub(b.lastUpdate).Nanoseconds() * b.rate
+	// Calculate the time elapsed since the last update, in nanoseconds
+	elapsedTime := time.Now().Sub(b.lastUpdate).Nanoseconds()
+
+	// Calculate the amount of tokens have leaked over the elapsed time
+	r := elapsedTime * int64(b.rate)
+
+	// Update the remaining number tokens
+	b.remaining -= uint(r)
 
 	// Ensure remaining is no less than zero
 	if b.remaining < 0 {
--- rate_limiting/bucket_test.go
+++ rate_limiting/bucket_test.go
@@ -1,1 +1,62 @@
 package rate_limiting
+
+import (
+	"testing"
+	"time"
+)
+
+func TestCreate(t *testing.T) {
+	b := Create(10, 0.000000000001389)
+
+	if b.capacity != 10 {
+		t.Errorf("Create() generated Bucket with incorrect capacity\n\treceived: %v\n\texpected: %v", b.capacity, 10)
+	}
+
+	if b.remaining != 10 {
+		t.Errorf("Create() generated Bucket with incorrect remaining\n\treceived: %v\n\texpected: %v", b.remaining, 10)
+	}
+
+	if b.rate != 0.000000000001389 {
+		t.Errorf("Create() generated Bucket with incorrect remaining\n\treceived: %v\n\texpected: %v", b.remaining, 0.000000000001389)
+	}
+
+	if !time.Now().Equal(b.lastUpdate) {
+		t.Errorf("Create() generated Bucket with incorrect lastUpdate\n\treceived: %v\n\texpected: %v", b.lastUpdate, time.Now())
+	}
+}
+
+func TestCapacity(t *testing.T) {
+	b := Create(10, 0.000000000001389)
+
+	if b.Capacity() != 10 {
+		t.Errorf("Capacity() returned incorrect capacity\n\treceived: %v\n\texpected: %v", b.Capacity(), 10)
+	}
+}
+
+func TestRemaining(t *testing.T) {
+	b := Create(10, 0.000000000001389)
+
+	if b.Remaining() != 10 {
+		t.Errorf("Remaining() returned incorrect remaining\n\treceived: %v\n\texpected: %v", b.Remaining(), 10)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	b := Create(10, 0.000000000001389)
+
+	result := make(chan bool)
+
+	b.mux.Lock()
+
+	go func() {
+		b.Add(15)
+		result <- true
+	}()
+
+	select {
+	case <-result:
+		t.Errorf("Add() did not correctly lock the thread")
+	case <-time.After(10 * time.Second):
+		return
+	}
+}
