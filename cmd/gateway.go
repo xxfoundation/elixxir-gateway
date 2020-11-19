319cc209101acfb9ec8aa5e970d4827f7806f0b2
Benjamin Wenger <ben@elixxir.ioo>
2020-11-19 09:02:19 -0800

    added logs and made gateway rewport errors in processing unified polls

--- cmd/gateway.go
+++ cmd/gateway.go
@@ -194,7 +194,7 @@ func PollServer(conn *gateway.Comms, pollee *connect.Host, ndf,
 		GatewayPort:    uint32(gwPort),
 		GatewayVersion: currentVersion,
 	}
-
+	jww.DEBUG.Printf("Polling Server: %+v", pollMsg)
 	resp, err := conn.SendPoll(pollee, pollMsg)
 	return resp, err
 }
@@ -220,6 +220,7 @@ func CreateNetworkInstance(conn *gateway.Comms, ndf, partialNdf *pb.NDF) (
 // UpdateInstance reads a ServerPollResponse object and updates the instance
 // state accordingly.
 func (gw *Instance) UpdateInstance(newInfo *pb.ServerPollResponse) error {
+	jww.DEBUG.Printf("Server poll response: %+v", newInfo)
 	// Update the NDFs, and update the round info, which is currently
 	// recorded but not used for anything. (maybe we should print state
 	// of each round?)
@@ -720,7 +721,9 @@ func (gw *Instance) Start() {
 					err)
 				continue
 			}
-			gw.UpdateInstance(msg)
+			if err = gw.UpdateInstance(msg); err != nil {
+				jww.WARN.Printf("Failed to update instance: %+v", err)
+			}
 		}
 	}()
 }
