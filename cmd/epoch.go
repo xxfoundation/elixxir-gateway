492ff55b4e2f4c0662a9c30568ebb89dabbcee7d
josh <josh@elixxir.io>
2021-07-13 09:38:58 -0700

    Clean up code

---
+++ cmd/epoch.go
@@ -0,0 +1,47 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright © 2020 xx network SEZC                                          //
+//                                                                           //
+// Use of this source code is governed by a license that can be found in the //
+// LICENSE file                                                              //
+///////////////////////////////////////////////////////////////////////////////
+
+package cmd
+
+import (
+	"github.com/pkg/errors"
+	jww "github.com/spf13/jwalterweatherman"
+)
+
+// GetEpochEdge determines the Epoch value of the given timestamp with the
+// given period while returning an error. To be used when either of the
+// inputs come from the network.
+func GetEpochEdge(ts int64, period int64) (uint32, error) {
+	if period == 0 {
+		return 0, errors.New("GetEpochEdge: Period length is 0, " +
+			"cannot divide by zero")
+	} else if ts < 0 {
+		return 0, errors.Errorf("GetEpochEdge: Cannot calculate "+
+			"epoch with a negative timestamp: %d", ts)
+	} else if period < 0 {
+		return 0, errors.Errorf("GetEpochEdge: Cannot calculate "+
+			"epoch with a negative period size: %d", period)
+	}
+	return uint32(ts / period), nil
+}
+
+// GetEpoch determines the Epoch value of the given timestamp
+// with the given period. Panics on error. For internal use
+func GetEpoch(ts int64, period int64) uint32 {
+	epoch, err := GetEpochEdge(ts, period)
+
+	if err != nil {
+		jww.FATAL.Panicf("%+v", err)
+	}
+
+	return epoch
+}
+
+// GetEpochTimestamp determines the timestamp value of the given epoch
+func GetEpochTimestamp(epoch uint32, period int64) int64 {
+	return period * int64(epoch)
+}
--- cmd/gateway_test.go
+++ cmd/gateway_test.go
@@ -1443,43 +1443,6 @@ func TestInstance_ClearOldStorage(t *testing.T) {
 }
 
 // Happy path
-// Can't test panic paths, obviously
-func TestGetEpoch(t *testing.T) {
-	ts := int64(300000)
-	period := int64(5000)
-	expected := uint32(60)
-	result := GetEpoch(ts, period)
-	if result != expected {
-		t.Errorf("Invalid GetEpoch result: Got %d Expected %d", result, expected)
-	}
-}
-
-// Various happy paths
-func TestGetEpochTimestamp(t *testing.T) {
-	epoch := uint32(60)
-	period := int64(5000)
-	expected := int64(300000)
-	result := GetEpochTimestamp(epoch, period)
-	if result != expected {
-		t.Errorf("Invalid GetEpochTimestamp result: Got %d Expected %d", result, expected)
-	}
-
-	period = 0
-	expected = 0
-	result = GetEpochTimestamp(epoch, period)
-	if result != expected {
-		t.Errorf("Invalid GetEpochTimestamp result: Got %d Expected %d", result, expected)
-	}
-
-	period = -5000
-	expected = -300000
-	result = GetEpochTimestamp(epoch, period)
-	if result != expected {
-		t.Errorf("Invalid GetEpochTimestamp result: Got %d Expected %d", result, expected)
-	}
-}
-
-// Happy path
 func TestInstance_SetPeriod(t *testing.T) {
 	gw := NewGatewayInstance(Params{DevMode: true})
 	err := gw.SetPeriod()
