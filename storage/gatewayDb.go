fe1e7cb0d55931ae2c85dbf7568c9f644302ed98
Jake Taylor <jake@elixxir.io>
2022-01-27 13:55:51 -0600

    fix db query

--- storage/gatewayDb.go
+++ storage/gatewayDb.go
@@ -232,7 +232,7 @@ func (d *DatabaseImpl) GetClientBloomFilters(recipientId ephemeral.Id, startEpoc
 	var results []*ClientBloomFilter
 	recipientIdInt := recipientId.Int64()
 	err := d.db.WithContext(ctx).Where("epoch BETWEEN ? AND ?", startEpoch, endEpoch).Find(&results, &ClientBloomFilter{RecipientId: &recipientIdInt}).Error
-	jww.INFO.Printf("Returning filters [%v] for client [%v]", results, recipientId)
+	jww.DEBUG.Printf("Returning filters [%v] for client [%v]", results, recipientId)
 
 	return results, catchErrors(err)
 }
