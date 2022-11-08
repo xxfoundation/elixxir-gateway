38894f3ef43072c41619a5d82f66f01e6bad6f4f
Richard T. Carback III <rick.carback@gmail.com>
2022-11-08 21:49:06 +0000

    Add final tests

--- autocert/dns_test.go
+++ autocert/dns_test.go
@@ -10,8 +10,10 @@ package autocert
 import (
 	"bytes"
 	"crypto"
+	"encoding/pem"
 	"fmt"
 	"math/rand"
+	"os"
 	"testing"
 
 	"golang.org/x/crypto/acme"
@@ -23,6 +25,7 @@ func TestMain(m *testing.M) {
 	dnsClientObj = func() acmeClient {
 		return &dummyACMEClient{}
 	}
+	os.Exit(m.Run())
 }
 
 func TestGenerateCertTestGenerate(t *testing.T) {
@@ -91,7 +94,7 @@ func TestRegister(t *testing.T) {
 	}
 
 	// dGVzdHN0cmluZwo= = "teststring"
-	err = n.Register(key, "eabKeyID", "dGVzdHN0cmluZwo=",
+	err = n.Register(key, "eabKeyID", "dGVzdHN0cmluZwo",
 		"email@example.com")
 	if err != nil {
 		t.Errorf("%+v", err)
@@ -105,6 +108,118 @@ func TestRegister(t *testing.T) {
 		t.Errorf("key not set, byte mismatch:\n%v\n%v\n",
 			key.MarshalPem(), actualN.PrivateKey.MarshalPem())
 	}
+}
+
+func TestRequest(t *testing.T) {
+	n := NewDNS()
+	rng1 := &dummyRNG{}
+	key, err := GenerateCertKey(rng1)
+	if err != nil {
+		t.Errorf("%+v", err)
+	}
+
+	// dGVzdHN0cmluZwo= = "teststring"
+	err = n.Register(key, "eabKeyID", "dGVzdHN0cmluZwo",
+		"email@example.com")
+	if err != nil {
+		t.Errorf("%+v", err)
+	}
+	k, v, err := n.Request("example.com")
+	if err != nil {
+		t.Errorf("%+v", err)
+	}
+	expK := "_acme-challenge.example.com"
+	if k != expK {
+		t.Errorf("unexpected key: expect %s, got %s", expK, k)
+	}
+	if v != "dnsChallengeString" {
+		t.Errorf("unexpect chal: expected dnsChallengeString got %s",
+			v)
+	}
+
+	actualN := n.(*dnsClient)
+	if actualN.GetKey() == nil {
+		t.Errorf("internal key not set")
+	}
+	if bytes.Compare(actualN.PrivateKey.MarshalPem(),
+		key.MarshalPem()) != 0 {
+		t.Errorf("key not set, byte mismatch:\n%v\n%v\n",
+			key.MarshalPem(), actualN.PrivateKey.MarshalPem())
+	}
+	if actualN.AuthzURL != "url1" {
+		t.Errorf("bad AuthzURL: %s", actualN.AuthzURL)
+	}
+	if actualN.Domain != "example.com" {
+		t.Errorf("bad domain: %s", actualN.Domain)
+	}
+	if actualN.AuthzFinalizeURL != "finalme" {
+		t.Errorf("bad final URL: %s", actualN.AuthzFinalizeURL)
+	}
+}
+
+func TestCSR(t *testing.T) {
+	n := NewDNS()
+	rng1 := &dummyRNG{}
+	key, err := GenerateCertKey(rng1)
+	if err != nil {
+		t.Errorf("%+v", err)
+	}
+
+	// dGVzdHN0cmluZwo= = "teststring"
+	err = n.Register(key, "eabKeyID", "dGVzdHN0cmluZwo",
+		"email@example.com")
+	if err != nil {
+		t.Errorf("%+v", err)
+	}
+
+	csrPEM, csrDER, err := n.CreateCSR("example.com", "test@example.com",
+		"USA", "nodeID", rng1)
+	if err != nil {
+		t.Errorf("%+v", err)
+	}
+
+	expPEM := []byte{45, 45, 45, 45, 45, 66, 69, 71, 73, 78, 32, 67, 69,
+		82, 84, 73, 70, 73, 67, 65}
+	if bytes.Compare(expPEM, csrPEM[:len(expPEM)]) != 0 {
+		t.Errorf("bad pem:\n\t%v\n\t%v", expPEM, csrPEM[:len(expPEM)])
+	}
+	expDER := []byte{48, 130, 4, 143, 48, 130, 2, 119, 2, 1, 0, 48, 74,
+		49, 12, 48, 10, 6}
+	if bytes.Compare(expDER, csrDER[:len(expDER)]) != 0 {
+		t.Errorf("bad der:\n\t%v\n\t%v", expDER, csrDER[:len(expDER)])
+	}
+}
+
+func TestIssue(t *testing.T) {
+	n := NewDNS()
+	rng1 := &dummyRNG{}
+	key, err := GenerateCertKey(rng1)
+	if err != nil {
+		t.Errorf("%+v", err)
+	}
+
+	// dGVzdHN0cmluZwo= = "teststring"
+	err = n.Register(key, "eabKeyID", "dGVzdHN0cmluZwo",
+		"admins@elixxir.io")
+	if err != nil {
+		t.Errorf("%+v", err)
+	}
+	_, _, err = n.Request("rick1.xxn2.work")
+	if err != nil {
+		t.Errorf("%+v", err)
+	}
+
+	_, csrDER, err := n.CreateCSR("rick1.xxn2.work", "admins@elixxir.io",
+		"USA", "nodeID", rng1)
+	if err != nil {
+		t.Errorf("%+v", err)
+	}
+
+	_, _, err = n.Issue(csrDER)
+	if err != nil {
+		t.Errorf("%+v", err)
+	}
+
 }
 
 type dummyACMEClient struct {
@@ -137,15 +252,70 @@ func (a *dummyACMEClient) DNS01ChallengeRecord(token string) (string, error) {
 }
 func (a *dummyACMEClient) AuthorizeOrder(ctx context.Context,
 	authzIDs []acme.AuthzID) (*acme.Order, error) {
-	return &acme.Order{}, nil
+	return &acme.Order{
+		AuthzURLs: []string{
+			"url1",
+		},
+		FinalizeURL: "finalme",
+	}, nil
 }
 func (a *dummyACMEClient) CreateOrderCert(ctx context.Context,
 	finalURL string, csr []byte, ty bool) ([][]byte, string, error) {
-	return nil, "", nil
+	validCert := `-----BEGIN CERTIFICATE-----
+MIIHbjCCBVagAwIBAgIQSM+Uht+j3A/bOj7JPRBDrzANBgkqhkiG9w0BAQwFADBL
+MQswCQYDVQQGEwJBVDEQMA4GA1UEChMHWmVyb1NTTDEqMCgGA1UEAxMhWmVyb1NT
+TCBSU0EgRG9tYWluIFNlY3VyZSBTaXRlIENBMB4XDTIyMTEwNTAwMDAwMFoXDTIz
+MDIwMzIzNTk1OVowGjEYMBYGA1UEAxMPcmljazEueHhuMi53b3JrMIICIjANBgkq
+hkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAxn0SZaCiduE3RINmtOI2GsmO4jGputtF
+RhV9c8SwbnHZvVsZGJpcF0Zp/ONhSbIiPJEzVoNUylwTNp4JFr1ePkigLOrR4akJ
+ovmAE1GzhNuBvgyxjS05e/hgGzDno186r9WaxcDnuMqaXF3syGEGoyDkB4hSJmY3
+YOya8KrxZNHw7IDGOIjoyWCz8XzBvXR5KGztSgixG28IYkB+wAyfHj9IrRf0/T3Y
+u6CYbBgwD5JLEG2Mn5WLbDfcIDubNxB4ZrxoFKUfZGfoankrO3C7cPjn+JgC8ZZz
+kNN1KlD1ZqkNDPI077UvVhhBhbPQehYcEbCwLBSoXQRvVn7Ij4OROIqgTJI5wBIo
+FrmYfgbI4xjEgYBfLS3u+FUvM7KrCh9IaFC5gtJSxsqGHkxsAYYEyXNqys/yvCEb
+fCBMH08zmoHhqLbvQNIrCY+L31C4jcvS/FaqGiWjusljUW5/1FKi8SnHCWrneAxN
+oKlsWHTYbgppf9N42Yy25w/Yp4n7cpUoFmCio6L9KLsZsRcWGuX9kzdcklqVOsuF
+Ej+XnOdAVujXbgAkvtlyxlcQky1n9PZ2jxOnyQQ22+9PuGVz9DMI6MIrkScSfWYO
+BDN6Lb6O3CZ0DRl3h1c7KUlCssoNcqi67G7vpSuRYMQ7TvSaxZIpf4pIepufrUiV
+FXJSdrupTwMCAwEAAaOCAn0wggJ5MB8GA1UdIwQYMBaAFMjZeGii2Rlo1T1y3l8K
+Pty1hoamMB0GA1UdDgQWBBTvyeRInS6KVVyO9eK82Fy8Jl2o7TAOBgNVHQ8BAf8E
+BAMCBaAwDAYDVR0TAQH/BAIwADAdBgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUH
+AwIwSQYDVR0gBEIwQDA0BgsrBgEEAbIxAQICTjAlMCMGCCsGAQUFBwIBFhdodHRw
+czovL3NlY3RpZ28uY29tL0NQUzAIBgZngQwBAgEwgYgGCCsGAQUFBwEBBHwwejBL
+BggrBgEFBQcwAoY/aHR0cDovL3plcm9zc2wuY3J0LnNlY3RpZ28uY29tL1plcm9T
+U0xSU0FEb21haW5TZWN1cmVTaXRlQ0EuY3J0MCsGCCsGAQUFBzABhh9odHRwOi8v
+emVyb3NzbC5vY3NwLnNlY3RpZ28uY29tMIIBBgYKKwYBBAHWeQIEAgSB9wSB9ADy
+AHcArfe++nz/EMiLnT2cHj4YarRnKV3PsQwkyoWGNOvcgooAAAGERYbgFQAABAMA
+SDBGAiEA/d+USH39vf8RvckdgiB+a+NyDEb/7xG4VIBZPYIXLQACIQD4XySXos55
+o0wyE3XTzylkqoyupOaLtGpSkbDZlIKp2QB3AHoyjFTYty22IOo44FIe6YQWcDIT
+hU070ivBOlejUutSAAABhEWG3+MAAAQDAEgwRgIhAOBKcYLCr3EhrTqyywMvCA3Z
+OeYesLfOdpJJJgkGB9aVAiEA36DwB++0dEzJf6JiXsXpuUfjZ4KgtDWufl+FYNd3
+LlgwGgYDVR0RBBMwEYIPcmljazEueHhuMi53b3JrMA0GCSqGSIb3DQEBDAUAA4IC
+AQAon7p8PMR/dbGBzqG0zoNotB+BxqdJV1VtJk0XYKQl/i03fBr4fzvhnBEFvKe4
+qjk2YUqB52muLl64n0o0yI96p74F5j+X1xbuX1JwT1hA3udGRNrS4vkFTUV/ymhe
+CPWnZaSpIudtb5uO/nNCZ/+794NHzmPHbC4oTo83wRFoxZst50jvC2a5E9ewY41h
+uZ0la/n6Q+2/F9CBYYPvLeLqPmRco9QhXl6CDndvetwNkOXh75Kt517Xu97TcdK2
+S+UdpRcZWonxosNxLEFtu3otRmxzT/3PhMit3GqQxw3gFzqjNIgwmumF2buSGZeH
+SVBh+HkqxyE286UtHanCPrYv4ev4WjV05PvRbwSZyx/d//Lvmo9lheXzKN4HVVbh
+wZX1HDu+mkmeHquDAQYT3Wbn0f5rmXaCbCBtAOLDpJs8jEu/9Xsb8R9IgsBdUkeL
+tPt8haNAF82yYKlcBFJhPsQZSSabMy1Ew6gXqDCLC+YyxY5ASuLk4fOBMqfGuVDS
+9Po+PFUvdas/yypuMauZWazj2kXki06xGylTb5pDgResTuGMQ4X7+9COzoGLNedt
+iz2njhHe7DWAQiOFOllO1mCUvzJ0oyYF4C9YHIT4j+Mzq31mmpLAcv/oI10Yaco9
+ha+P46sxUx4cDvtzq29TynRlbNAXj27yripo/2Azn6IG/Q==
+-----END CERTIFICATE-----`
+	pemBlk, _ := pem.Decode([]byte(validCert))
+	der := pemBlk.Bytes
+	return [][]byte{der}, "certURL", nil
 }
 func (a *dummyACMEClient) GetAuthorization(ctx context.Context,
 	authzURL string) (*acme.Authorization, error) {
-	return &acme.Authorization{}, nil
+	return &acme.Authorization{
+		Challenges: []*acme.Challenge{
+			{
+				Type: "dns-01",
+			},
+		},
+	}, nil
 }
 func (a *dummyACMEClient) Accept(ctx context.Context,
 	chal *acme.Challenge) (*acme.Challenge, error) {
@@ -153,7 +323,9 @@ func (a *dummyACMEClient) Accept(ctx context.Context,
 }
 func (a *dummyACMEClient) WaitAuthorization(ctx context.Context,
 	authzURL string) (*acme.Authorization, error) {
-	return &acme.Authorization{}, nil
+	return &acme.Authorization{
+		Status: acme.StatusValid,
+	}, nil
 }
 
 type dummyRNG struct{}
